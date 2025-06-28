34a7a60e2bd9bd4f023639a3f8797d79d94e9a8e
lahcen404 <lahcen.maskour2003@gmail.com>
2025-06-28 16:46:20 +0100

    Create test project

---
+++ src/main/java/org/skypay/test_banking/TestBankingApplication.java
@@ -0,0 +1,13 @@
+package org.skypay.test_banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestBankingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestBankingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/skypay/test_banking/TestBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypay.test_banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestBankingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
